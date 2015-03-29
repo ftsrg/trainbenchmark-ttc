f910f7c48208da9cee5534ea63a0a634322cb65d
dwagelaar <dwagelaar@gmail.com>
2015-03-29 15:49:34 +0200

    Rename EMFTVM to ATL.

--- hu.bme.mit.trainbenchmark.ttc.benchmark.atl/src/test/java/hu/bme/mit/trainbenchmark/ttc/benchmark/atl/ATLTest.java
+++ hu.bme.mit.trainbenchmark.ttc.benchmark.atl/src/test/java/hu/bme/mit/trainbenchmark/ttc/benchmark/atl/ATLTest.java
@@ -1,11 +1,11 @@
-package hu.bme.mit.trainbenchmark.ttc.benchmark.emftvm;
+package hu.bme.mit.trainbenchmark.ttc.benchmark.atl;
 
 import hu.bme.mit.trainbenchmark.ttc.benchmark.atl.ATLBenchmarkLogic;
 import hu.bme.mit.trainbenchmark.ttc.benchmark.config.BenchmarkConfig;
 import hu.bme.mit.trainbenchmark.ttc.benchmark.scenarios.AbstractBenchmarkLogic;
 import hu.bme.mit.trainbenchmark.ttc.benchmark.test.TrainBenchmarkTest;
 
-public class EMFTVMTest extends TrainBenchmarkTest {
+public class ATLTest extends TrainBenchmarkTest {
 
 	@Override
 	protected AbstractBenchmarkLogic getBenchmarkLogic(final BenchmarkConfig bc) {
