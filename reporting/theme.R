26379c46300fccc93c1aae8ae7eebbf7a646f12f
Marton Bur <marton.bur@gmail.com>
2015-05-11 21:21:24 +0200

    Increased diagram font size

--- reporting/theme.R
+++ reporting/theme.R
@@ -4,7 +4,11 @@ bwTheme <- theme(text=element_text(family="Helvetica", size=16),
                  legend.direction="vertical",
                  panel.grid.major = element_line(size=0.3, colour="#333333"),
                  panel.grid.minor = element_line(size=0.15, colour="#CCCCCC"),
-                 axis.text.x = element_text(colour="black"),
-                 axis.text.y = element_text(colour="black"),
+                 axis.text.x = element_text(colour="black",size=25),
+				 axis.text.y = element_text(colour="black",size=25),
+				 axis.title = element_text(colour="black",size=25),
+				 legend.title = element_text(colour="black",size=20),
+				 legend.text = element_text(colour="black",size=20),
+				 plot.title = element_text(colour="black",size=30),
                  legend.position="right"
 )
