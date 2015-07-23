d3a90fbe91107bd0ded4823f667dd1c791e80a37
Gabor Szarnyas <szarnyasg@gmail.com>
2015-07-24 00:39:39 +0200

    Scoring scripts

--- reporting/theme.R
+++ reporting/theme.R
@@ -1,7 +1,7 @@
 bwTheme <- theme(text=element_text(family="Helvetica", size=16),
                  panel.background = element_rect(fill="#FFFFFF"),
                  legend.position="bottom",
-                 legend.direction="vertical",
+                 legend.direction="horizontal",
                  panel.grid.major = element_line(size=0.3, colour="#333333"),
                  panel.grid.minor = element_line(size=0.15, colour="#CCCCCC"),
                  axis.text.x = element_text(colour="black",size=20),
