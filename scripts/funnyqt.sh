9ee9363baa839cf329507246028ff9e4619b4fa4
Gabor Szarnyas <szarnyasg@gmail.com>
2015-07-30 18:31:36 +0200

    Updated dependency resolver script

--- scripts/funnyqt.sh
+++ scripts/funnyqt.sh
@@ -2,12 +2,14 @@
 
 pushd ..
 
-git clone https://github.com/tsdh/ttc15-train-benchmark-funnyqt.git
-cd ttc15-train-benchmark-funnyqt
+if [ ! -d neo4j-shell-tools ]; then
+	git clone https://github.com/tsdh/ttc15-train-benchmark-funnyqt.git
+fi
 
+cd ttc15-train-benchmark-funnyqt
 git pull
 # download lein and build the project
-wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
+wget -nc https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
 chmod +x lein
 ./lein install
 
